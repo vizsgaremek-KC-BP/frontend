0a64b8d5b8e4cdd8a8aad86b28f255830a6d8ad6
CintiaKiss <kisscintiaszimonetta@ktch.hu>
2025-02-07 14:29:40 +0100

    kezdeti fázisok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { AboutComponent } from './components/about/about.component';
+import { LibraryListComponent } from './components/library-list/library-list.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LogInComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'library', component: LibraryListComponent },
+  { path: 'register', component: RegistrationComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './components/home/home.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { AdminUserComponent } from './components/admin-user/admin-user.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { LibraryListComponent } from './components/library-list/library-list.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AboutComponent } from './components/about/about.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LogInComponent,
+    RegistrationComponent,
+    AdminUserComponent,
+    NavBarComponent,
+    LibraryListComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    AngularFireDatabaseModule,
+    HttpClientModule,
+    //beimportali a translation cuccost------------
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
